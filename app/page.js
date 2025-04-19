f344acd6273b3dd590c094859e1fc64e59835d59
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2025-04-18 21:24:01 -0700

    Testing language change buttons

--- app/page.js
+++ app/page.js
@@ -1,10 +1,18 @@
+'use client'
 import Image from 'next/image';
 import styles from './page.module.css';
 
+import {useContext} from 'react'
+import { LanguageContext } from './components/LanguageContext';
+
 export default function Home() {
+  const {language, updateLanguage} = useContext(LanguageContext);
+
   return (
     <div>
-      <main>{/* home page content goes here */}</main>
+      <main>
+        <p>{language=='English'?'Hello':'Hola'}</p>
+      </main>
     </div>
   );
 }
